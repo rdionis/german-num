ebdf02e7ff772cd7fe9c94757bad95054d48584a
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-02-19 14:02:26 +0100

    added index file with the question

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("PRINT EXPONENTIAL VALUES");
+// Write a function that accepts two numbers and validate that they are numbers.
+// After that, the function should print y exponential values starting from x.
+// For example if we have function(3, 5) The function should print 3 9 27 81 243. Prints 5 exponential values of 3.
+// function(2, 8) The function prints 2 4 8 16 32 64 128 256. Prints 8 exponential values of 2.
