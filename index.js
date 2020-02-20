c5405e9ca3264c059ca33cce76dbe2540b8682e7
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-02-20 16:34:47 +0100

    first solution for german number exercise

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-GERMAN PHONE NUMBER
+//GERMAN PHONE NUMBER
 
-// Create a function that receive a text from userand check if the user has entered a phone number, and print that out
+// Create a function that receive a text from user and check if the user has entered a phone number, and print that out
 // Note: phone number should be within the German network starts with (030,017)
 // and has 7 digits after the prefix number
 
-//HERE COMES THE FIRST SOLUTION
+function germanNum(userText) {
+  let userArr = userText.split(" ");
+  let result = "";
+  for (let i = 0; i < userArr.length; i++) {
+    let first3Chr = userArr[i].slice(0, 3);
+    if (first3Chr == "030" || first3Chr == "017") {
+      if (userArr[i].length == 10) {
+        result = userArr[i];
+      } else {
+        result = "This is NOT a valid number";
+      }
+    }
+  }
+  return result;
+}
+console.log(germanNum("afahglahgkla 0172546987 alkhflakhgka"));
+console.log(germanNum("afahglahgkla 01725469870000 alkhflakhgka"));
