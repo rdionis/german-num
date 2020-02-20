25128bd6828a052b206ab40d06aee94b95ff46d0
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-02-20 15:54:07 +0100

    changes to index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.log("PRINT EXPONENTIAL VALUES");
-// Write a function that accepts two numbers and validate that they are numbers.
-// After that, the function should print y exponential values starting from x.
-// For example if we have function(3, 5) The function should print 3 9 27 81 243. Prints 5 exponential values of 3.
-// function(2, 8) The function prints 2 4 8 16 32 64 128 256. Prints 8 exponential values of 2.
+GERMAN PHONE NUMBER
+
+// Create a function that receive a text from userand check if the user has entered a phone number, and print that out
+// Note: phone number should be within the German network starts with (030,017)
+// and has 7 digits after the prefix number
 
 //HERE COMES THE FIRST SOLUTION
